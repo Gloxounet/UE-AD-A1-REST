52a993f413969506b490c2e43e0ab7c72af7de41
gloxounet <matteo.glock@laposte.net>
2022-09-13 16:58:28 +0200

    commit bookink, movie and showtime (tested)

--- booking/booking.py
+++ booking/booking.py
@@ -16,6 +16,43 @@ def home():
    return "<h1 style='color:blue'>Welcome to the Booking service!</h1>"
 
 
+@app.route("/bookings", methods=['GET'])
+def get_json():
+   return make_response(jsonify(bookings),200)
+
+@app.route("/bookings/<userid>", methods=['GET'])
+def get_booking_for_user(userid):
+   for booking in bookings:
+      if str(booking["userid"]) == str(userid):
+         res = make_response(jsonify(booking),200)
+         return res
+   return make_response(jsonify({"error":"booking ID not found"}),400)
+
+@app.route("/bookings/<userid>", methods=['POST'])
+def add_booking_byuser(userid):
+   req = request.get_json()
+
+   for booking in bookings:
+      if str(booking["userid"]) == str(userid):
+         return make_response(jsonify({"error":"booking ID already exists"}),409)
+
+   res = {
+         "userid": userid,
+         "dates": [
+            {
+               "date":req["date"],
+               "movies":req["movieid"]
+            }
+         ]
+         }
+
+   bookings.append(res)
+
+
+   res = make_response(jsonify({"message":"booking added"}),200)
+   return res
+
+
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
--- showtime/showtime.py
+++ showtime/showtime.py
@@ -14,6 +14,23 @@ with open('{}/databases/times.json'.format("."), "r") as jsf:
 def home():
    return "<h1 style='color:blue'>Welcome to the Showtime service!</h1>"
 
+@app.route("/showtimes", methods=['GET'])
+def get_schedule():
+   return make_response(jsonify(schedule),200)
+
+@app.route("/showmovies/<date>")
+def get_movie_bydate(date):
+   date = str(date)
+   json = ""
+   for s in schedule :
+      if int(s["date"]) ==  int(date) :
+         json = s["movies"]
+   if json != "" :
+      return make_response(jsonify(json),200)
+
+   return make_response(jsonify({'error':'not found'}))
+
+
 if __name__ == "__main__":
    print("Server running in port %s"%(PORT))
    app.run(host=HOST, port=PORT)
